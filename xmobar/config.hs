fbbc9bccb46e1bb4dee114fd8033eb5f3f4272e3
rakeshkky <12475069+rakeshkky@users.noreply.github.com>
2019-10-18 18:51:06 +0530

    update xmobar config
    -> updated font (also bold now)
    -> removed few icons and replaced with font symbols

--- xmobar/config.hs
+++ xmobar/config.hs
@@ -1,6 +1,6 @@
 Config {
     position = Top,
-    font = "xft:Source Code Pro Medium:size=10:antialias=true",
+    font = "xft:Bitstream Vera Sans Mono:size=9:bold:antialias=true",
     bgColor = "#000000",
     fgColor = "#ffffff",
     lowerOnStart = False,
@@ -8,15 +8,15 @@ Config {
     allDesktops = True,
     persistent = True,
     commands = [
-          Run MultiCpu ["-t","<icon=/home/rakesh/Desktop/xmonad/xmobar/icons/cpu.xbm/><total0> <total1> <total2> <total3>","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10
-        , Run Memory ["-t","<icon=/home/rakesh/Desktop/xmonad/xmobar/icons/mem.xbm/> <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10
+          Run MultiCpu ["-t","♥<total0> <total1> <total2> <total3> ","-L","30","-H","60","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC","-w","3"] 10
+        , Run Memory ["-t","♠ <usedratio>%","-H","8192","-L","4096","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10
         , Run Swap ["-t","<usedratio>%","-H","1024","-L","512","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"] 10
         , Run DynNetwork [ "-t"
                        , "<icon=/home/rakesh/Desktop/xmonad/xmobar/icons/wifi_02.xbm/> <rx><icon=/home/rakesh/Desktop/xmonad/xmobar/icons/net_down_03.xbm/> <tx><icon=/home/rakesh/Desktop/xmonad/xmobar/icons/net_up_03.xbm/>"
                        , "-H" , "200" , "-L" , "10","-h","#FFB6B0","-l","#CEFFAC","-n","#FFFFCC"
                        ] 10
         , Run Date "%a %b %d<icon=/home/rakesh/Desktop/xmonad/xmobar/icons/clock.xbm/>%H:%M" "date" 10
-        , Run Battery        [ "--template" , "<icon=/home/rakesh/Desktop/xmonad/xmobar/icons/bat_full_01.xbm/> <acstatus>"
+        , Run Battery        [ "--template" , "⚡ <acstatus>"
                              , "--Low"      , "10"        -- units: %
                              , "--High"     , "80"        -- units: %
                              , "--low"      , "red"
@@ -27,11 +27,11 @@ Config {
                                        -- discharging status
                                        , "-o"	, "<left>% (<timeleft>)"
                                        -- AC "on" status
-                                       , "-O"	, "<fc=#dAA520>Charging</fc>"
+                                       , "-O"	, "<fc=#dAA520>Charging</fc> (<left>%)"
                                        -- charged status
-                                       , "-i"	, "<fc=#006000>Charged</fc>"
+                                       , "-i"	, "<fc=#006000>Charged</fc> (<left>%)"
                              ] 50
-        , Run Volume "default" "Master" ["-t", "<icon=/home/rakesh/Desktop/xmonad/xmobar/icons/spkr_03.xbm/> <volume>%"] 1
+        , Run Volume "default" "Master" ["-t", "♫ <volume>%"] 1
         , Run StdinReader
     ],
     sepChar = "%",
