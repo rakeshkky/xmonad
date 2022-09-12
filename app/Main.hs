a15c56057723d7a5c4a0633b20e039467ca08ed5
Rakesh <12475069+rakeshkky@users.noreply.github.com>
2022-09-12 13:20:41 +0530

    enable multimonitor setup

--- app/Main.hs
+++ app/Main.hs
@@ -229,15 +229,12 @@ myKeys conf @ XConfig { XMonad.modMask = modMask } = M.fromList
   [ ((m .|. modMask, k), windows $ f i)
     | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
     , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]
-  ]
-
-  -- Avoiding the following for dual only monitor setup
-  -- ++
+  ] ++
   -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
   -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
-  -- [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
-  --     | (key, sc) <- zip [xK_e, xK_w, xK_r] [0..]
-  --     , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
+  [((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+      | (key, sc) <- zip [xK_e, xK_w, xK_r] [0..]
+      , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 ------------------------------------------------------------------------
 -- Mouse bindings
 --
